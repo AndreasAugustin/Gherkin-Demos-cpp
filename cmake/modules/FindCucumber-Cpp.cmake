cf52418748aba624b847c1519c1230eff701982e
AndreasAugustin <andy.augustin@t-online.de>
2016-04-12 21:17:22 +0200

    refactoring CMAkeLists.txt

--- cmake/modules/FindCucumber-Cpp.cmake
+++ cmake/modules/FindCucumber-Cpp.cmake
@@ -1,7 +1,10 @@
 if(NOT Cucumber-Cpp_FOUND)
-    set(Cucumber-Cpp_ROOT_DIR ${CMAKE_MODULE_PATH}/../../vendor/cucumber-cpp)
-    add_subdirectory(${Cucumber-Cpp_ROOT_DIR} cucumbercpp)
-    set(Cucumber-Cpp_INCLUDE_DIRS ${Cucumber-Cpp_ROOT_DIR}/include)
+    set(_Cucumber-Cpp_ROOT_DIR ${CMAKE_MODULE_PATH}/../../vendor/cucumber-cpp)
+    add_subdirectory(${_Cucumber-Cpp_ROOT_DIR} cucumbercpp)
+    set(Cucumber-Cpp_INCLUDE_DIRS ${_Cucumber-Cpp_ROOT_DIR}/include)
     set(Cucumber-Cpp_LIBRARY Cucumber-Cpp)
     set(Cucumber-Cpp_FOUND True)
+
+    mark_as_advanced(
+            _Cucumber-Cpp_ROOT_DIR)
 endif()
--- cmake/modules/FindGMock.cmake
+++ cmake/modules/FindGMock.cmake
@@ -1,17 +1,21 @@
 
 if(NOT GMOCK_FOUND)
-    set(GOOGLETEST_DISTRIBUTION_ROOT_DIR ${CMAKE_MODULE_PATH}/../../vendor/googletest-distribution)
-    set(GMOCK_ROOT_DIR ${GOOGLETEST_DISTRIBUTION_ROOT_DIR}/googlemock)
-    add_subdirectory(${GMOCK_ROOT_DIR} gmock)
-    set(GMOCK_INCLUDE_DIR ${GMOCK_ROOT_DIR}/include)
+    set(_GOOGLETEST_DISTRIBUTION_ROOT_DIR ${CMAKE_MODULE_PATH}/../../vendor/googletest-distribution)
+    set(_GMOCK_ROOT_DIR ${_GOOGLETEST_DISTRIBUTION_ROOT_DIR}/googlemock)
+    add_subdirectory(${_GMOCK_ROOT_DIR} gmock)
+    set(GMOCK_INCLUDE_DIR ${_GMOCK_ROOT_DIR}/include)
     set(GMOCK_LIBRARY gmock)
     set(GMOCK_FOUND True)
 
-    set(GTEST_INCLUDE_DIRS ${GOOGLETEST_DISTRIBUTION_ROOT_DIR}/googletest/include)
+    set(GTEST_INCLUDE_DIRS ${_GOOGLETEST_DISTRIBUTION_ROOT_DIR}/googletest/include)
     set(GTEST_LIBRARY gtest)
     set(GTEST_FOUND True)
 
     set(GMOCK_INCLUDE_DIRS
             ${GMOCK_INCLUDE_DIR}
             ${GTEST_INCLUDE_DIRS})
+
+    mark_as_advanced(
+            _GOOGLETEST_DISTRIBUTION_ROOT_DIR
+            _GMOCK_ROOT_DIR)
 endif()
